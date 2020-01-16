dce35951f3e6452fe416101960ec2be4055eceb9
wi11e <willredmondson@gmail.com>
2020-01-16 12:20:19 +0000

    setup: docker, nest, mongo

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import {MongooseModule} from "@nestjs/mongoose";
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb://root:pass@mongo:27017/')],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
